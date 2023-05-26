ac0c56d78507a4cf9441d2688997531cf1d1be24
duv0-x <duvan.ballen@gmail.com>
2023-05-25 21:29:23 -0500

    Bucket owner, Access block, ACL, content type and  depends on property on object

--- first-steps/__main__.py
+++ first-steps/__main__.py
@@ -4,14 +4,34 @@ import pulumi
 from pulumi_aws import s3
 
 # Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+bucket = s3.Bucket("my-bucket",
+    website=s3.BucketWebsiteArgs(
+        index_document="index.html",
+    ),
+)
 
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
 
+ownership_controls = s3.BucketOwnershipControls(
+    'ownership-controls',
+    bucket=bucket.id,
+    rule=s3.BucketOwnershipControlsRuleArgs(
+        object_ownership='ObjectWriter',
+    ),
+)
+
+public_access_block = s3.BucketPublicAccessBlock(
+    'public-access-block', bucket=bucket.id, block_public_acls=False
+)
+
 # Create an S3 Bucket object
-bucketObject = s3.BucketObject(
+bucket_object = s3.BucketObject(
     'index.html',
     bucket=bucket.id,
-    source=pulumi.FileAsset('./index.html')
+    source=pulumi.FileAsset('index.html'),
+    content_type='text/html',
+    acl='public-read',
+    opts=pulumi.ResourceOptions(depends_on=[public_access_block]),
 )
+
