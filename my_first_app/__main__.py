b280ed3d68a4c8f859c53b2c73dba99f7dedf471
duv0-x <duvan.ballen@gmail.com>
2023-05-30 21:24:04 -0500

    creating frontend and mongo images

--- my_first_app/__main__.py
+++ my_first_app/__main__.py
@@ -4,6 +4,13 @@ import os
 import pulumi
 import pulumi_docker as docker
 
+# Get configuration values
+config = pulumi.Config()
+frontend_port = config.require_int("frontendPort")
+backend_port = config.require_int("backendPort")
+mongo_port = config.require_int("mongoPort")
+
+
 stack = pulumi.get_stack()
 
 # Pull the backend image
@@ -11,3 +18,14 @@ backend_image_name = "backend"
 backend = docker.RemoteImage(f"{backend_image_name}_image",
                              name="pulumi/tutorial-pulumi-fundamentals-backend:latest"
                              )
+
+# Pull the frontend image
+frontend_image_name = "frontend"
+frontend = docker.RemoteImage(f"{frontend_image_name}_image",
+                              name="pulumi/tutorial-pulumi-fundamentals-frontend:latest"
+                              )
+
+# Pull the MongoDB image
+mongo_image = docker.RemoteImage("mongo_image",
+                                 name="pulumi/tutorial-pulumi-fundamentals-database-local:latest"
+                                 )
